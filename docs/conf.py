7e9b937acc8edd8bfca86d63a71ca1ca4ee5f1ac
Michael Neylon <neylon_michael_t@lilly.com>
2018-05-07 10:05:38 -0400

    rtd mock pyspark.sql

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'pandas', 'pyspark', 'fastparquet']
+MOCK_MODULES = ['numpy', 'pandas', 'pyspark', 'pyspark.sql', 'fastparquet']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
