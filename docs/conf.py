c5d52403d39f0086d9a20f810c0d0b61c4891a5f
Michael Neylon <neylon_michael_t@lilly.com>
2018-05-07 10:01:40 -0400

    rtd py2 import mock

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-from unittest.mock import MagicMock
+from mock import Mock as MagicMock
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
